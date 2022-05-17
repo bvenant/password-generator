d5cebcadf4a9709c162057482a9829aa057d7dc5
Bradley V <bvenant@aol.com>
2022-05-17 14:06:16 -0400

    add parameter elements

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 // Assignment code here
+const length = document.querySelector("length").checked;
+const lowercase = document.querySelector("lowercase").checked;
+const uppercase = document.querySelector("uppercase").checked;
+const number = document.querySelector("number").checked;
+const character = document.querySelector("character").checked;
+  
+//One box needs to be chaecked
+var group = length + lowercase + uppercase + number + character;
+
+if (group === 0) { generatedPassword = generatePassword + "Please check at least one!";
+    return generatePassword;
+  };
+
+  // Password Parameters
+    const tall = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+    const short = "abcdefghijklmnopqrstuvwxyz".split('');
+    const  unit= "0123456789".split('');
+    const symbol = "!@#$%^&*()_+~\\`|}{[]:;?><,./-=".split('');
+
+
 
 
 // Get references to the #generate element
